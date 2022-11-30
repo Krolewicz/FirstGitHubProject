9e6d47ffed0769341f353207437452555951329a
Królewicz <bartek.konon2@wp.pl>
2022-11-30 12:30:46 +0100

    test

--- FirstGitHubProject/Program.cs
+++ FirstGitHubProject/Program.cs
@@ -7,6 +7,7 @@ namespace FirstGitHubProject
         static void Main(string[] args)
         {
             Console.WriteLine("Hello World!");
+            Console.WriteLine("Dodatkowa linia");
         }
     }
 }
